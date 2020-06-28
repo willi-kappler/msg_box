91dc5286b3132c00e920ff28d62708a98f5a4fa8
Willi Kappler <willi.kappler.gm@gmail.com>
2020-06-29 00:34:23 +0200

    Export error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod msg_box;
 
-pub use crate::msg_box::{MsgData, MsgBox, new_msg_box, add_new_receiver, remove_receiver, send_message, get_next_message,
+pub use crate::msg_box::{MsgData, MsgBox, MsgError, new_msg_box, add_new_receiver, remove_receiver, send_message, get_next_message,
     add_new_group, remove_group, add_receiver_to_group, send_message_to_group
 };
